85a54fbff8b95ec7611f93eac43b9c5514fbac56
Jonathan Lo <jonathanlo411@gmail.com>
2023-02-08 19:37:21 -0800

    add workshops iframe

---
+++ src/routes/workshops/+page.svelte
@@ -0,0 +1,8 @@
+<iframe title="workshops" src="https://brianjhuang.github.io/ds3-workshops23/"></iframe>
+<style>
+    iframe {
+        width:100%;
+        height:100vh;
+        overflow:hidden;
+    }
+</style>
