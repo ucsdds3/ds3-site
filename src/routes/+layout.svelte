04644d80d252775ef7f52e1c77b75b93907aad52
Jonathan Lo <jonathanlo411@gmail.com>
2023-02-09 09:16:48 -0800

    modify nav for fixed pos

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,19 +13,34 @@
     </div>
 </nav>
 <slot></slot>
+<footer>
+    <h2 id="footer-title">Connect with us</h2>
+    <div id="socials-section">
+
+    </div>
+</footer>
+
 
 <style global>
     @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
     :global(body) {
+        /* General Styling */
         box-sizing: border-box;
         margin: 0%;
+
+        /* Color vars */
+        --ds3-orange: #f58134;
+        --ds3-blue: #11b3c9;
+        --ds3-gray: #2e2e2e;
     }
   
     nav {
         background-color: #f8f8f8;
         width: 100%;
-        height: 5vh;
+        height: 6vh;
         font-family: 'Montserrat', Verdana, Geneva, Tahoma, sans-serif;
+        position: fixed;
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
     }
     #img-wrapper {
         display: flex;
@@ -46,7 +61,7 @@
     .nav-item {
         display: table-cell;
         vertical-align: middle;
-        font-size: 19px;
+        font-size: 23px;
         text-decoration: none;
         padding-right: 1.75vw;
         color: black;
