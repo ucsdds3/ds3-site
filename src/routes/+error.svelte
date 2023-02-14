e492a611a633256786b3a25e058cd9de2e98465a
Jonathan Lo <jonathanlo411@gmail.com>
2023-02-14 12:03:28 -0800

    add error message for modal refresh

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+     
+    import { page } from '$app/stores';
+</script>
+
+<section>
+    <h1>Uh oh, looks like an error occured</h1>
+</section>
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+    section {
+        background-color: var(--base-bg);
+        margin-top: 10vh;
+        height: 70vh;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-family: 'Montserrat';
+        justify-content: space-around;
+        font-size: 3vh;
+        text-align: center;
+    }
+</style>
