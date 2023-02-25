890dd31384888985e2cb19e40b994bed2c705f2d
Jonathan Lo <jonathanlo411@gmail.com>
2023-02-24 22:56:29 -0800

    patching

--- src/routes/events/calendarInterfaces.ts
+++ src/routes/events/calendarInterfaces.ts
@@ -41,7 +41,7 @@ interface CleanEvent {
     title: string,
     date: string,
     time: string,
-    image: string,
+    image?: string,
     location: {
         mapsLocation?: string,
         roomLocation?: string
