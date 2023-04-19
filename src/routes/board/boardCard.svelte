a235308f3d03200be9f901fbbf37fd83bfe1c764
CodingKomodo <ecao7777@gmail.com>
2023-04-18 23:19:28 -0700

    Added Media Queries for Modal
    Edited the Modal View with media queries for large Screens

--- src/routes/board/boardCard.svelte
+++ src/routes/board/boardCard.svelte
@@ -176,6 +176,32 @@
         margin: 0 3%;
         padding: 3%;
     }
+
+    @media (min-width: 1500px) {
+        .modal #modal-pfp {
+            width: 30vh;
+            height: 30vh;
+            top: -27vh;
+        }
+        .modal-info p {
+            font-size: 1.25rem;
+        }
+        .modal-info h1 {
+            font-size: 2.5rem;
+        }
+        .modal-info h2 {
+            font-size: 1.75rem;
+        }
+        .modal-links {
+            /* height: 30%; */
+        }
+        .modal-links a {
+            font-size: 1.5rem;
+            padding: 6%;
+            /* width: 75%; */
+        }
+    }
+
     .orange { background-color: var(--ds3-orange); }
     .blue { background-color: var(--ds3-blue); }
     .grey { background-color: var(--ds3-gray); }
