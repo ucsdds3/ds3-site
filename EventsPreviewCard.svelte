2cbddf4b42a860755b9b632e00a34a4ab209ee56
Nav-neets <118164684+Nav-neets@users.noreply.github.com>
2023-05-03 12:18:09 -0700

    Create EventsPreviewCard.svelte

---
+++ EventsPreviewCard.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let title = 'DS3 UCSD';
+  export let description = `This is a test for events preview.`;
+</script>
+
+<svelte:head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{title}</title>
+  <meta name="description" content="{description}">
+  <meta property="og:title" content="{title}">
+  <meta property="og:description" content="{description}">
+  <meta property="og:image" content="/favicon.png">
+  <meta property="og:url" content="https://www.ds3ucsd.com/">
+  <meta property="og:type" content="website">
+</svelte:head>
