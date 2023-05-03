f1680354dacc8639ac2729fc3e5138b9350df394
Nav-neets <118164684+Nav-neets@users.noreply.github.com>
2023-05-03 10:41:41 -0700

    Update +page.svelte

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,20 +1,3 @@
-<script>
-  export let title = 'DS3 UCSD';
-   export let description = `Developing students' professionalism through 30+ networking events, technical skill building events, recruiting sessions, career information events, and more!.`;
-</script>
-
-<svelte:head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>{title}</title>
-  <meta name="description" content="{description}">
-  <meta property="og:title" content="{title}">
-  <meta property="og:description" content="{description}">
-  <meta property="og:image" content="/favicon.png">
-  <meta property="og:url" content="https://www.ds3ucsd.com/">
-  <meta property="og:type" content="website">
-</svelte:head>
-
 <script lang='ts'>
     import type { PageData } from './$types';
     import EventCard from './eventCard.svelte';
