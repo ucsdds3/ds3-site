971fe2e15ad94e198edb1706f375c6b9987818c1
Nav-neets <118164684+Nav-neets@users.noreply.github.com>
2023-05-03 14:05:02 -0700

    Update +page.svelte

--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -2,7 +2,23 @@
     import type { PageData } from './$types';
     
     export let data: PageData;
+    
+    export let title = 'Upcoming Events - DS3 UCSD';
+    
+    export let description = `Test for sponsors.`;
 </script>
+
+<svelte:head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{title}</title>
+  <meta name="description" content="{description}">
+  <meta property="og:title" content="{title}">
+  <meta property="og:description" content="{description}">
+  <meta property="og:image" content="/favicon.png">
+  <meta property="og:url" content="https://www.ds3ucsd.com/events">
+  <meta property="og:type" content="website">
+  </svelte:head>
 
 <section>
     <div class="title-sec">
