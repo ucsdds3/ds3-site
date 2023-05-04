d771b14d030162b349ca2c69b5ebf2eb0020001c
Jonathan Lo <72477459+jonathanlo411@users.noreply.github.com>
2023-05-04 09:29:10 -0700

    Update +page.svelte

--- src/routes/get-involved/+page.svelte
+++ src/routes/get-involved/+page.svelte
@@ -16,7 +16,7 @@
   <meta property="og:image" content="/favicon.png">
   <meta property="og:url" content="https://www.ds3ucsd.com/get-involved">
   <meta property="og:type" content="website">
-  </svelte:head>
+</svelte:head>
 
 <section>
     <div class="title-sec">
